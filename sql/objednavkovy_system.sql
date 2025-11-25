91879e2855925c888beb1f28c3a65a8d48e95cfd
Tomáš Klepač <74875406+tomasklepac@users.noreply.github.com>
2025-11-25 13:07:34 +0100

    Updated database export

--- sql/objednavkovy_system.sql
+++ sql/objednavkovy_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Počítač: 127.0.0.1
--- Vytvořeno: Pát 03. říj 2025, 21:51
+-- Vytvořeno: Úte 25. lis 2025, 13:06
 -- Verze serveru: 10.4.32-MariaDB
 -- Verze PHP: 8.2.12
 
@@ -20,9 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Databáze: `objednavkovy_system`
 --
-
-CREATE DATABASE IF NOT EXISTS `objednavkovy_system` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE `objednavkovy_system`;
 
 -- --------------------------------------------------------
 
@@ -47,16 +44,18 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `customer_id`, `status`, `note`, `total_cents`, `created_at`, `street`, `city`, `zip`) VALUES
-(4, 3, 'shipped', '', 49800, '2025-09-30 21:02:06', '', '', ''),
-(5, 3, 'confirmed', '', 1180, '2025-09-30 21:02:47', '', '', ''),
+(4, 3, 'delivered', '', 49800, '2025-09-30 21:02:06', '', '', ''),
+(5, 3, 'delivered', '', 1180, '2025-09-30 21:02:47', '', '', ''),
 (6, 3, 'delivered', '', 24900, '2025-10-01 09:27:44', '', '', ''),
-(7, 3, 'confirmed', '', 590, '2025-10-01 09:29:09', '', '', ''),
-(8, 5, 'shipped', '', 23580, '2025-10-01 09:38:01', '', '', ''),
+(7, 3, 'delivered', '', 590, '2025-10-01 09:29:09', '', '', ''),
+(8, 5, 'delivered', '', 23580, '2025-10-01 09:38:01', '', '', ''),
 (9, 5, 'delivered', '', 590, '2025-10-02 07:23:31', '', '', ''),
 (10, 5, 'confirmed', '', 13310, '2025-10-02 14:12:40', '', '', ''),
-(11, 5, 'confirmed', '', 9950, '2025-10-02 16:28:24', 'Komenského 149', 'Starý Plzenec', '33202'),
+(11, 5, 'shipped', '', 9950, '2025-10-02 16:28:24', 'Komenského 149', 'Starý Plzenec', '33202'),
 (12, 5, 'canceled', '', 590, '2025-10-02 16:29:24', 'Komenského 149', 'Starý Plzenec', '33202'),
-(13, 5, 'confirmed', '', 2490, '2025-10-02 16:49:29', 'Komenského 149', 'Starý Plzenec', '33202');
+(13, 5, 'delivered', '', 2490, '2025-10-02 16:49:29', 'Komenského 149', 'Starý Plzenec', '33202'),
+(14, 3, 'delivered', '', 5750, '2025-11-20 22:54:41', 'Nová 25', 'Bukovec', '22345'),
+(20, 3, 'shipped', 'Prosim dorucit co nejdrive', 15990, '2025-11-23 16:42:20', 'Komenského 149', 'Plzen', '33202');
 
 -- --------------------------------------------------------
 
@@ -89,7 +88,10 @@ INSERT INTO `order_item` (`order_id`, `product_id`, `quantity`, `unit_price_cent
 (10, 4, 4, 590),
 (11, 1, 5, 1990),
 (12, 4, 1, 590),
-(13, 2, 1, 2490);
+(13, 2, 1, 2490),
+(14, 1, 2, 1990),
+(14, 4, 3, 590),
+(20, 7, 1, 15990);
 
 -- --------------------------------------------------------
 
@@ -114,9 +116,11 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `description`, `price_cents`, `stock`, `image_path`, `supplier_id`, `is_active`, `created_at`) VALUES
-(1, 'Jablko červené', 'Čerstvé šťavnaté jablko z farmy', 1990, 8, 'uploads/prod_68e00fcb6d2193.29855128.jpg', 2, 1, '2025-09-27 14:47:35'),
-(2, 'Hruška žlutá', 'Sladká hruška z Jižní Moravy', 2490, 9, 'uploads/prod_68e011c80f0430.53612348.jpg', 2, 1, '2025-09-27 19:13:37'),
-(4, 'Švestka fialová', 'Mňam', 590, 6, 'uploads/prod_68e014125b3083.52865953.jpg', 7, 1, '2025-09-29 18:34:09');
+(1, 'Jablko červené', 'Čerstvé šťavnaté jablko z farmy', 1990, 5, 'uploads/prod_68e00fcb6d2193.29855128.jpg', 2, 1, '2025-09-27 14:47:35'),
+(2, 'Hruška žlutá', 'Sladká hruška z Jižní Moravy', 2490, 2, 'uploads/prod_68e011c80f0430.53612348.jpg', 2, 1, '2025-09-27 19:13:37'),
+(4, 'Švestka fialová', 'Mňam', 590, 3, 'uploads/prod_68e014125b3083.52865953.jpg', 7, 1, '2025-09-29 18:34:09'),
+(7, 'Víno', 'Z moravských svahů', 15990, 3, 'uploads/prod_69232f2d0c0a87.55601391.jpg', 2, 1, '2025-11-23 15:58:01'),
+(8, 'asdfg', 'sdfghjkl', 3900, 2, 'uploads/prod_6925974a4fdce9.70878377.webp', 2, 1, '2025-11-25 11:47:22');
 
 -- --------------------------------------------------------
 
@@ -126,7 +130,7 @@ INSERT INTO `products` (`id`, `name`, `description`, `price_cents`, `stock`, `im
 
 CREATE TABLE `roles` (
   `id` int(11) NOT NULL,
-  `code` varchar(50) NOT NULL
+  `code` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -134,36 +138,10 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id`, `code`) VALUES
-(1, 'admin'),
-(2, 'supplier'),
-(3, 'customer'),
-(4, 'super_admin');
-
--- --------------------------------------------------------
-
---
--- Struktura tabulky `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password_hash` varchar(255) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `is_active` tinyint(1) NOT NULL DEFAULT 1,
-  `is_approved` tinyint(1) NOT NULL DEFAULT 1,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Vypisuji data pro tabulku `users`
---
-
-INSERT INTO `users` (`id`, `email`, `password_hash`, `name`, `is_active`, `is_approved`, `created_at`) VALUES
-(1, 'admin@local.test', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'Admin User', 1, 1, '2025-09-30 20:50:00'),
-(2, 'supplier@local.test', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'Petr Novák', 1, 1, '2025-09-30 20:50:00'),
-(3, 'customer@local.test', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'Zákazník', 1, 1, '2025-09-30 20:50:00'),
-(8, 'superadmin@local.test', '$2y$12$r5OwxikzyH/6VbbDx7QMluWl.UDpWbXyCukA4cB04J8M1y3GKcuwm', 'SuperAdmin', 1, 1, '2025-11-21 12:00:00');
+(3, 'admin'),
+(1, 'customer'),
+(4, 'super_admin'),
+(2, 'supplier');
 
 -- --------------------------------------------------------
 
@@ -191,7 +169,9 @@ INSERT INTO `users` (`id`, `email`, `password_hash`, `name`, `is_active`, `is_ap
 (3, 'customer@local.test', '$2y$10$WLTKTS.qz6L2ONdKx/OvZ.KhY99i.IBKVVNWVmEe6nIpkCRqsLmpW', 'Zákazník', 1, 1, '2025-09-27 19:13:13'),
 (5, 'tomasklepac@post.cz', '$2y$10$S0LBa9RYgmQRqVJhL2HAjuEgRUrTZn6i3C3VnKgZSiGmmbj1KGw7a', 'Tomáš Klepač', 1, 1, '2025-09-29 17:19:35'),
 (7, 'petrnovak@local.test', '$2y$10$442oRYMbiPcw2IjktG2dVuO/NoHPwE3FRDBdjc.LmSPgNo2er69C6', 'Petr Novák', 1, 1, '2025-09-29 18:33:04'),
-(8, 'superadmin@local.test', '$2y$10$K9Uw6xGZHqX5V8mN2pQ7feL0R3S4T5U6V7W8X9Y0Z1A2B3C4D5E6', 'SuperAdmin', 1, 1, '2025-10-01 10:00:00');
+(8, 'superadmin@local.test', '$2y$12$r5OwxikzyH/6VbbDx7QMluWl.UDpWbXyCukA4cB04J8M1y3GKcuwm', 'SuperAdmin', 1, 1, '2025-11-21 11:27:36'),
+(9, 'admin2@local.test', '$2y$10$YBWgWlXAyGDDJ1oSJnIipujfzeVz0jqEKJlXXVpfeCzl/j0mQdzGO', 'Admin2', 1, 1, '2025-11-21 11:40:39'),
+(10, 'karelmalik@local.test', '$2y$10$kBRum2xfPmdvHg7VXEOlaea/HzxWlugKLjusoPxqR38DcjTfHUj92', 'Karel Malík', 1, 1, '2025-11-21 11:45:34');
 
 -- --------------------------------------------------------
 
@@ -214,7 +194,9 @@ INSERT INTO `user_role` (`user_id`, `role_id`) VALUES
 (3, 1),
 (5, 1),
 (7, 2),
-(8, 4);
+(8, 4),
+(9, 3),
+(10, 1);
 
 --
 -- Indexy pro exportované tabulky
@@ -270,25 +252,25 @@ ALTER TABLE `user_role`
 -- AUTO_INCREMENT pro tabulku `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT pro tabulku `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT pro tabulku `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pro tabulku `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Omezení pro exportované tabulky
