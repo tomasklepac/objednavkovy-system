2208e58a6cd47a2336dfa7e14199cbeded003377
Tomáš Klepač <74875406+tomasklepac@users.noreply.github.com>
2025-11-25 10:58:16 +0100

    Modified directory

--- public/api.php
+++ public/api.php
@@ -17,7 +17,7 @@
 session_start();
 
 // Load autoloader
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../app/autoload.php';
 
 // Use API controller
 use App\Controllers\ApiController;
